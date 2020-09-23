de3db7f7fe783a668e11835a863f3453ef6fc9a2
Your Name <you@example.com>
2020-09-23 16:28:28 -0400

    updated assigned variables to local files

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Password-Generator
+
+## Description 
+
+This employee password generator is a simple, conscise javascript codebase. It's mainly built from strings, prompts, confirms, while loops and if/else conditional statements. Once the user access the page, the password generator prompts a question, asking the user how many characters they want in their password. The prompt is powered through a while loop telling the user to choose their desired password length between 8 to 128 characters. An alert is built in through an if else condition informing users to they have to stay within the password length range. Proceeding after the prompt is a series of confirms giving users their choice of character types through a series of boolean if else statements, OK (true) and Cancel (false). After clicking through all the confirms, the users password will be genereated of their desired length. 
+
+## Screenshots
+
+## Sources
+
+Codepen: https://codepen.io/rennan/pen/BEYZGX?css-preprocessor=none
+
+MDN Web Docs: https://developer.mozilla.org/en-US/
+
+Stackoverflow: https://stackoverflow.com/
+
+W3schools: https://www.w3schools.com/default.asp
+
+Youtube: https://www.youtube.com/watch?v=fOvYE1i_IiY
+
+## URL
+
+## Repo URL
--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,57 @@
-// Assignment Code
+// Assigned Variables
+var charLower = "abcdefghijklmnopqrstuvwxyz";
+var charUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var charNum = "0123456789"
+var charSpecial = "!@#$%^&*()_+-{}.[],/:'<?>;=";
+var password = generatePassword();
+var passwordText = document.querySelector("#password");
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  
 
+  // Create password length between 8-128 characters
+
+  var passwordLength = for (var i = 0; i > 128; i++ || var i = 0; i < 8; i++);
+  alert("Password length must be between 8 and 128 characters");
+    writePassword();
+    return passwordLength
+  }
+ 
+  // Create character types to include (uppercase, lowercase, numeric, special characters)
   passwordText.value = password;
 
 }
-
-function generatePassword{
+var passwordLength = prompt(
+  "How many characters would you like your password to contain?"
+  );
+var charAlpha = confirm(
+  "Click OK to confirm including letters."
+  );
+var charNum = confirm(
+  "Click OK to confirm including numbers."
+  );
+var charSpecial = confirm(
+  "Click OK to confirm including special characters."
+  );
+var charUpper  = confirm(
+  "Click OK to confirm including uppercase letters."
+  );
+var charLower = confirm(
+  "Click OK to confirm including lowercase letters."
+  );
+function generatePassword() {
+  if 
   return "This will be replaced by my generated password.";
 }
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// Tools
+// Conditions (if, else statements)
+// Use Math.random() to generate random numbers
+// Arrays
+// Loops (for loop to iterate through arrays)
