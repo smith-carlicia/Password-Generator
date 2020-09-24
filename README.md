3dcbc27131a00b6ec7ffd4decb81df643db3d252
Your Name <you@example.com>
2020-09-23 23:58:36 -0400

    update local files

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 
 ## Description 
 
-This employee password generator is a simple, conscise javascript codebase. It's mainly built from strings, prompts, confirms, while loops and if/else conditional statements. Once the user access the page, the password generator prompts a question, asking the user how many characters they want in their password. The prompt is powered through a while loop telling the user to choose their desired password length between 8 to 128 characters. An alert is built in through an if else condition informing users to they have to stay within the password length range. Proceeding after the prompt is a series of confirms giving users their choice of character types through a series of boolean if else statements, OK (true) and Cancel (false). After clicking through all the confirms, the users password will be genereated of their desired length. 
+This employee password generator is a simple, conscise javascript codebase. It's mainly built from strings, prompts, confirms, while loops and if/else conditional statements. Once the user access the page, the password generator prompts a question, asking the user how many characters they want in their password. The prompt is powered through an if conditional statement telling the user to choose their desired password length between 8 to 128 characters. An alert is built in through an if conditional statement informing users to they have to stay within the password length range. Proceeding after the prompt is a series of confirms giving users their choice of character types through a series of boolean if else statements, OK (true) and Cancel (false). After clicking through all the confirms, the users password will be genereated of their desired length. 
 
 ## Screenshots
+Password-Generator.png
 
 ## Sources
 
@@ -21,3 +22,4 @@ Youtube: https://www.youtube.com/watch?v=fOvYE1i_IiY
 ## URL
 
 ## Repo URL
+https://github.com/smith-carlicia/Password-Generator.git
--- assets/script.js
+++ assets/script.js
@@ -1,57 +1,91 @@
-// Assigned Variables
-var charLower = "abcdefghijklmnopqrstuvwxyz";
-var charUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var charNum = "0123456789"
-var charSpecial = "!@#$%^&*()_+-{}.[],/:'<?>;=";
+//Arrays for each possible choice
+var charAlpha = [];
+var charLower = ["a","b","c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var charUpper = ["A","B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var charNum = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var charSpecial = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "{", "}", ".", "[", "]", ",", "/", ":", "'", "<", "?", ">", ";", "="];
 var password = generatePassword();
 var passwordText = document.querySelector("#password");
 var generateBtn = document.querySelector("#generate");
 
+
+// Grab the users choice - create a function that will prompt the user with their options
+ ///// chars set - upper or lower case - interger option - special char
+// Condtioanl statements - confrim user choice 
+function askMe () {
+    var passwordLength = parseInt(
+     prompt("How many characters would you like your password to contain?");
+    );
+
+    // Conditional - will check the user input 
+    if(isNaN(passwordLength) === true){
+        alert("Type a character length between 8 - 128 characters.")
+    }
+    if (passwordLength < 8){
+        alert ('Password length must be more than 8 characters.')
+    }
+    if (passwordLength > 128) {
+         alert('Password length must be lower than 128 characters.')
+    };
+    //  Confirms -
+};
+function generatePassword() {
+    var charAlpha = confirm(
+        "Click OK to confirm including letters."
+    );
+    var charNum = confirm(
+        "Click OK to confirm including numbers."
+    );
+    var charSpecial = confirm(
+        "Click OK to confirm including special characters."
+    );
+    var charUpper  = confirm(
+        "Click OK to confirm including uppercase letters."
+    );
+    var charLower = confirm(
+        "Click OK to confirm including lowercase letters."
+    );
+    
+    if (charNum) {
+        charAlpha = charAlpha.concat(charNum);
+    }
+    if (charSpecial) {
+        charAlpha = charAlpha.concat(charSpecial);
+    }
+    if (charUpper) {
+        charAlpha = charAlpha.concat(charUpper);
+    }
+    if (charLower) {
+        charAlpha = charAlpha.concat(charLower);
+    }
+    if (charNum) {
+      charAlpha += charNum;
+    }
+    if (charSpecial) {
+      charAlpha += charSpecial;
+    }
+    if (charUpper) {
+      charAlpha += charUpper;
+    }
+    if (charLower) {
+      charLower += charLower;
+    }
+}
+
+// 4. Create  a random function  - 
+function random() {
+  var passwordLength = 
+  for (var i = 0; i < 128; i++) {
+    var charNum = Math.floor(Math.random() * 128) + 1;
+    console.log("charNum");
+    return "This will be replaced by my generated password.";
+
+
 // Write password to the #password input
 function writePassword() {
-  
-
-  // Create password length between 8-128 characters
-
-  var passwordLength = for (var i = 0; i > 128; i++ || var i = 0; i < 8; i++);
-  alert("Password length must be between 8 and 128 characters");
-    writePassword();
-    return passwordLength
+    var password = generatePassword();
+    var passwordText = document.querySelector('#password');
+    passwordText.value = password;
   }
- 
-  // Create character types to include (uppercase, lowercase, numeric, special characters)
-  passwordText.value = password;
-
-}
-var passwordLength = prompt(
-  "How many characters would you like your password to contain?"
-  );
-var charAlpha = confirm(
-  "Click OK to confirm including letters."
-  );
-var charNum = confirm(
-  "Click OK to confirm including numbers."
-  );
-var charSpecial = confirm(
-  "Click OK to confirm including special characters."
-  );
-var charUpper  = confirm(
-  "Click OK to confirm including uppercase letters."
-  );
-var charLower = confirm(
-  "Click OK to confirm including lowercase letters."
-  );
-function generatePassword() {
-  if 
-  return "This will be replaced by my generated password.";
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-// Tools
-// Conditions (if, else statements)
-// Use Math.random() to generate random numbers
-// Arrays
-// Loops (for loop to iterate through arrays)
+  // Add event listener to generate button
+  generateBtn.addEventListener('click', writePassword);
